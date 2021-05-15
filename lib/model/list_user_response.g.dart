2861a06aca4b208d12c0274aa242232667a6db71
Mahdi <82409174+01mahdivalipoor@users.noreply.github.com>
2021-05-15 22:05:38 +0430

    null safety

--- lib/model/list_user_response.g.dart
+++ lib/model/list_user_response.g.dart
@@ -8,14 +8,13 @@ part of 'list_user_response.dart';
 
 ListUserResponse _$ListUserResponseFromJson(Map<String, dynamic> json) {
   return ListUserResponse()
-    ..page = json['page'] as int
-    ..perPage = json['per_page'] as int
-    ..total = json['total'] as int
-    ..totalPages = json['total_pages'] as int
-    ..users = (json['data'] as List)
-        ?.map(
-            (e) => e == null ? null : User.fromJson(e as Map<String, dynamic>))
-        ?.toList();
+    ..page = json['page'] as int?
+    ..perPage = json['per_page'] as int?
+    ..total = json['total'] as int?
+    ..totalPages = json['total_pages'] as int?
+    ..users = (json['data'] as List<dynamic>?)
+        ?.map((e) => User.fromJson(e as Map<String, dynamic>))
+        .toList();
 }
 
 Map<String, dynamic> _$ListUserResponseToJson(ListUserResponse instance) =>
