4d76977f8318405adec76c8a1045df6660288cf9
Mahdi <82409174+01mahdivalipoor@users.noreply.github.com>
2021-05-15 22:10:52 +0430

    null safety

--- lib/model/user.g.dart
+++ lib/model/user.g.dart
@@ -8,11 +8,11 @@ part of 'user.dart';
 
 User _$UserFromJson(Map<String, dynamic> json) {
   return User()
-    ..id = json['id'] as int
-    ..email = json['email'] as String
-    ..firstName = json['first_name'] as String
-    ..lastName = json['last_name'] as String
-    ..avatar = json['avatar'] as String;
+    ..id = json['id'] as int?
+    ..email = json['email'] as String?
+    ..firstName = json['first_name'] as String?
+    ..lastName = json['last_name'] as String?
+    ..avatar = json['avatar'] as String?;
 }
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
